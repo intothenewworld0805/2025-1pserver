261165e56ba717d9481f66c8d3a5f3a064f326b9
108-0 <genie3002@naver.com>
2025-03-10 16:01:46 +0900

    KneighborsClassifier 를 사용한 붓꽃분류

--- irisModelBase.py
+++ irisModelBase.py
@@ -2,10 +2,14 @@
 # 프로젝트2 븟꽃분류기 만들기
 # 이용희교수님과 열심히 만들어보자
 
-from sklearn.neighbors import KNeighborsClassifier
+from fileinput import filename
 
+import joblib
 import pandas as pd
 import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 iris_df = pd.read_csv('iris.csv')
 # print(iris_df)
@@ -14,16 +18,24 @@ y = iris_df['species']
 X = iris_df.drop('species', axis = 1)
 
 kn = KNeighborsClassifier()
+rfc = RandomForestClassifier()
 model_kn = kn.fit(X, y)
+model_rfc = rfc.fit(X, y)
+
+joblib.dump(model_rfc, 'model_rfc.pkl')
 
 # X_new = np.array([[3, 3, 3, 3]])
 # kn ['versicolor'] [[0. 0.8 0.2]]
-#X_new = np.array([[5.0, 3.4, 3.5, 0.2]])
+X_new = np.array([[5.0, 3.4, 3.5, 0.2]])
 # kn ['setosa'] # [[1. 0. 0]]
-X_new = np.array([[1, 4.2, 1.4, 7]])
-prediction = model_kn.predict(X_new)
+# rfc ['setosa'] # [[1. 0. 0]]
+# rfc ['setosa'] [[0.53 0.21 0.26]]
+#X_new = np.array([[1, 4.2, 1.4, 7]])
+# prediction = model_kn.predict(X_new)
+prediction = model_rfc.predict(X_new)
 print(prediction)
-probability = model_kn.predict_proba(X_new)
+# probability = model_kn.predict_proba(X_new)
+probability = model_rfc.predict_proba(X_new)
 print(probability)
 # print(y)
 # print(X)
---
+++ main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+
+from irisModel import IrisMachineLEarning, IrisSpecies
+
+app = FastAPI()
+
+model = IrisMachineLEarning()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, this is iris classfier 2025/03/10"}
+
+@app.get("/predict")
+async def predict():
+    pred = model.predict_species(8, 1, 8, 1)
+    return {"prediction": pred[0]}
+
+@app.post("/predict")
+async def predict_species(iris:IrisSpecies):
+    pred = model.predict_species(iris.sepal_length, iris.sepal_width, iris.petal_length, iris.petal_width)
+    return pred
+
+if __name__== '__main__':
+    uvicorn.run(app, host = '127.0.0.1', port = 8000)
