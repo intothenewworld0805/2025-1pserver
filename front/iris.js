70460c81dffd70ecb145096c3077fece5b22516a
108-0 <genie3002@naver.com>
2025-03-17 15:34:42 +0900

    KneighborsClassifier 를 사용한 붓꽃분류 fastAPI 활용하여 get 까지 완료
    0317
    붓꽃분류기 프론트 html 까지 완료

---
+++ front/iris.js
@@ -0,0 +1,30 @@
+function Send() {
+
+    s1 = document.getElementById("sl")
+    sw = document.getElementById("sw")
+    pl = document.getElementById("pl")
+    pw = document.getElementById("pw")
+
+    var data = {
+        "sepal_length": 8,
+        "sepal_width": 1,
+        "petal_length": 8,
+        "petal_width": 1
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8000/predict",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        data: JSON.stringify(data)
+    }).done(function(response){
+        console.log(response)
+        txtOut.value = response.prediction
+    }).fail(function(response){
+        alert("fail" + response)
+    }).always()
+
+}
